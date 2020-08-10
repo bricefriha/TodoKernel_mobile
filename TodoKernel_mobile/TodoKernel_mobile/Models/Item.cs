02a44c0360eebe44f5aeaffd4005d1a9597c96d9
Brice Friha <brice.friha@aol.fr>
2020-08-10 18:18:50 +0200

    I created a item model

---
+++ TodoKernel_mobile/TodoKernel_mobile/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TodoKernel_mobile.Models
+{
+    public class Item
+    {
+        [JsonProperty("name")]
+        public string Title;
+
+        [JsonProperty("done")]
+        public string Done;
+    }
+}
