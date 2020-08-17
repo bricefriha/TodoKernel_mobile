89fc0d9140524db4d80fc48aa95ff5f139a756da
Brice Friha <brice.friha@aol.fr>
2020-08-17 19:14:10 +0200

    Create the form to add an item

--- TodoKernel_mobile/TodoKernel_mobile/Views/ItemsPage.xaml.cs
+++ TodoKernel_mobile/TodoKernel_mobile/Views/ItemsPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoKernel_mobile.Viewmodels;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +17,38 @@ namespace TodoKernel_mobile.Views
         {
             InitializeComponent();
             BindingContext = new TodolistViewModel();
+        }
+
+        private void Open_Form(object sender, EventArgs e)
+        {
+            // Get screen info
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+
+            
+            // Annimation
+            Action<double> callback = input => AddItemForm.HeightRequest = input;
+            double startHeight = 0;
+            double endHeight = mainDisplayInfo.Height / 15;
+            uint rate = 32;
+            uint length = 500;
+            Easing easing = Easing.CubicOut;
+            AddItemForm.Animate("anim", callback, startHeight, endHeight, rate, length, easing);
+
+            //
+            //if (MyDraggableView.Height == 0)
+            //{
+            //}
+            //else
+            //{
+            //    Action<double> callback = input => MyDraggableView.HeightRequest = input;
+            //    double startHeight = mainDisplayInfo.Height / 3;
+            //    double endiendHeight = 0;
+            //    uint rate = 32;
+            //    uint length = 500;
+            //    Easing easing = Easing.SinOut;
+            //    MyDraggableView.Animate("anim", callback, startHeight, endiendHeight, rate, length, easing);
+
+            //}
         }
     }
 }
